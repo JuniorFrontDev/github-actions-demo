16f9354cb370b0ccc97efc74d0585bc89ee3c928
JuniorFrontDev <47061092+JuniorFrontDev@users.noreply.github.com>
2025-02-14 12:47:46 +0000

    Deploying to gh-pages from @ JuniorFrontDev/github-actions-demo@40733457d46719639fb86d9292b019734e1f8558 🚀

---
+++ app.js
@@ -0,0 +1,7 @@
+function suma(a, b) {
+  return a + b;
+}
+
+// document.getElementById("result").textContent = suma(2, 2);
+
+module.exports = suma;
---
+++ app.test.js
@@ -0,0 +1,9 @@
+const suma = require("./app");
+
+test("suma 2 + 2 debe ser 4", () => {
+  expect(suma(2, 2)).toBe(4);
+});
+
+test("suma 0 + 0 debe ser 0", () => {
+  expect(suma(0, 0)).toBe(0);
+});
